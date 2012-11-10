6d0cf7564cfe398ce5ba40fb461ed85657e19469
Adam McNicol <adamsorders@mac.com>
2012-11-10 17:57:01 +0000

    overridden grow method in potato class
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- potato_class.py
+++ potato_class.py
@@ -10,6 +10,20 @@ class Potato (Crop):
 		super().__init__(1,3,6)
 		self._type = "Potato"
 
+	#override grow method for subclass
+	def grow(self,light,water):
+		if light >= self._light_need and water >= self._water_need:
+			if self._status == "Seedling" and water > self._water_need:
+				self._growth += self._growth_rate * 1.5
+			elif self._status == "Young" and water > self._water_need:
+				self._growth += self._growth_rate *1.25
+			else:
+				self._growth += self._growth_rate
+		#increment days growing
+		self._days_growing += 1
+		#update the status
+		self._update_status()
+
 
 def main():
 	#create a new potato crop
