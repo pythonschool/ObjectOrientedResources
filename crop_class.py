eaedd2b23cbead98bba752d3118f43dfdfbf6920
Adam McNicol <adamsorders@mac.com>
2012-11-10 17:08:05 +0000

    add code to instantiate the crop

--- crop_class.py
+++ crop_class.py
@@ -4,7 +4,7 @@ class Crop:
 	#constructor
 	def __init__(self,growth_rate,light_need,water_need):
 		#set the attributes with an initial value
-		
+
 		self._growth       = 0
 		self._days_growing = 0
 		self._growth_rate  = growth_rate
@@ -15,3 +15,16 @@ class Crop:
 
 		#the above attributes are prefixed with an underscore to indicate that they should not be
 		#accessed directly from outwith the class
+
+
+def main():
+	#instaniate the class
+	new_crop = Crop(1,4,3)
+	#test to see whether it works or not
+	print(new_crop._status)
+	print(new_crop._light_need)
+	print(new_crop._water_need)
+
+
+if __name__ == '__main__':
+	main()
