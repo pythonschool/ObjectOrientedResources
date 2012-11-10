6e1b484c67467a8eb07dd1a0956a9a7617127ecc
Adam McNicol <adamsorders@mac.com>
2012-11-10 17:54:45 +0000

    added main() function to potato crop to test it
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- potato_class.py
+++ potato_class.py
@@ -9,3 +9,18 @@ class Potato (Crop):
 		#growth rate = 1; light need = 3, water need = 6
 		super().__init__(1,3,6)
 		self._type = "Potato"
+
+
+def main():
+	#create a new potato crop
+	potato_crop = Potato()
+	print(potato_crop.report())
+	#manually grow the crop
+	manual_grow(potato_crop)
+	print(potato_crop.report())
+	auto_grow(potato_crop,15)
+	print(potato_crop.report())
+
+
+if __name__ == '__main__':
+	main()
