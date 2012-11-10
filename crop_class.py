3351869124ad3fb56acfb28b8b90757bccabd030
Adam McNicol <adamsorders@mac.com>
2012-11-10 17:21:16 +0000

    added update status and grow methods to crop_class
    updated main() to test the new methods

--- crop_class.py
+++ crop_class.py
@@ -26,16 +26,35 @@ class Crop:
 		#return a dictionary containing type,status,growth and days growing
 		return {'type':self._type,'status':self._status,'growth':self._growth,'days growing':self._days_growing}
 
+	#the underscore indicates that this method should not be called from outwith the class
+	def _update_status(self):
+		if self._growth > 15:
+			self._status = "Old"
+		elif self._growth > 10:
+			self._status = "Mature"
+		elif self._growth > 5:
+			self._status = "Young"
+		elif self._growth > 0:
+			self._status = "Seedling"
+		elif self._growth == 0:
+			self._status = "Seed"
 
+	def grow(self,light,water):
+		if light >= self._light_need and water >= self._water_need:
+			self._growth += self._growth_rate
+		#increment days growing
+		self._days_growing += 1
+		#update the status
+		self._update_status()
 
 def main():
 	#instaniate the class
 	new_crop = Crop(1,4,3)
 	#test to see whether it works or not
-	print(new_crop._status)
-	print(new_crop._light_need)
-	print(new_crop._water_need)
-
+	print(new_crop.needs())
+	print(new_crop.report())
+	print(new_crop.grow(4,4))
+	print(new_crop.report())
 
 if __name__ == '__main__':
 	main()
