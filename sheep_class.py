631d85fd894ef4681f3f8a9b554c87467ff1eec0
Adam McNicol <adamsorders@mac.com>
2012-11-29 09:47:10 +0000

    added sheep class and fix to cow
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- sheep_class.py
+++ sheep_class.py
@@ -1,19 +1,19 @@
 from animal_class import *
 
-class Cow(Animal):
-	"""A simulation of a Cow"""
+class Sheep(Animal):
+	"""A simulation of a Sheep"""
 
 	#constructor
 	def __init__(self,name):
-		#call the parent class constructor with default values for Cow
-		#growth rate = 2; food need = 5, water need = 4
-		super().__init__(2,5,4,name)
-		self._type = "Cow" #override the parent class attribute with new value
+		#call the parent class constructor with default values for Sheep
+		#growth rate = 3; food need = 3, water need = 2
+		super().__init__(3,3,2,name)
+		self._type = "Sheep" #override the parent class attribute with new value
 
 	#override the grow method from the parent class
 	def grow(self,food,water):
 		if food > self._food_need and water > self._water_need:
-			self._weight += self._growth_rate * 1.1
+			self._weight += self._growth_rate * 1.2
 		elif food == self._food_need and water == self._water_need:
 			self._weight += self._growth_rate
 		#increment days growing
@@ -23,9 +23,9 @@ class Cow(Animal):
 
 def main():
 	#instaniate the class
-	new_cow = Cow("Jim")
+	new_Sheep = Sheep("Shaun")
 	#test to see whether it works or not
-	manage_animal(new_cow)
+	manage_animal(new_Sheep)
 
 if __name__ == '__main__':
 	main()
