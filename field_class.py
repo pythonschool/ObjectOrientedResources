53c7e878970d6c6dc166691cab8029ce0efd29b8
Adam McNicol <adamsorders@mac.com>
2012-11-29 10:27:23 +0000

    added field class
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ field_class.py
@@ -0,0 +1,22 @@
+from potato_class import *
+from wheat_class import *
+from animal_class import *
+
+class Field:
+	"""Simulate a field that can contain animals and crops"""
+
+	#constructor
+	def __init__(self,max_animals,max_crops):
+		self._crops = []
+		self._animals = []
+		self._max_animals = max_animals
+		self._max_crops = max_crops
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
+	
