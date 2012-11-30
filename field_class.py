782b18d628ebb2326e98939ecf331f507119d2e8
Adam McNicol <adamsorders@mac.com>
2012-11-30 12:09:55 +0000

    added functions to manage crop fully
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- field_class.py
+++ field_class.py
@@ -176,17 +176,140 @@ def remove_animal_from_field(new_field):
 	selected_animal = select_animal(len(new_field._animals))
 	return new_field.remove_animal(selected_animal)
 
+def display_crop_menu():
+	print()
+	print("Which crop type would you like to add?")
+	print()
+	print("1. Potato")
+	print("2. Wheat")
+	print()
+	print("0. I don't want to add a crop - return me to the main menu")
+	print()
+	print('Please select an option from the above menu')
+
+def plant_crop_in_field(new_field):
+	display_crop_menu()
+	crop_choice = get_menu_choice(0,2)
+	if crop_choice == 1:
+		if new_field.plant_crop(Potato()):
+			print()
+			print("Crop planted")
+			print()
+		else:
+			print()
+			print("Wheat is full - wheat not planted")
+			print()
+	if crop_choice == 2:
+		if new_field.plant_crop(Wheat()):
+			print()
+			print("Wheat planted")
+			print()
+		else:
+			print()
+			print("Field is full - wheat not planted")
+			print()
+
+def add_animal_to_field(new_field):
+	display_animal_menu()
+	animal_choice = get_menu_choice(0,2)
+	name = input("Please give your new animal a name: ")
+	if animal_choice == 1:
+		if new_field.add_animal(Cow(name)):
+			print()
+			print("Cow added")
+			print()
+		else:
+			print()
+			print("Field is full - cow not added")
+			print()
+	if animal_choice == 2:
+		if new_field.add_animal(Sheep(name)):
+			print()
+			print("Sheep added")
+			print()
+		else:
+			print()
+			print("Field is full - sheep not planted")
+			print()
+
+
+def display_main_menu():
+    print('1. Plant a new crop')
+    print('2. Harvest a crop')
+    print()
+    print('3. Add an animal')
+    print('4. Remove an animal')
+    print()
+    print('5. Grow field manually over 1 day')
+    print('6. Grow field automatically over 30 days')
+    print()
+    print('7. Report field status')
+    print()
+    print('0. Exit test program')
+    print()
+    print('Please select an option from the above menu')
+
+def get_menu_choice(lower,upper):
+    optionValid = False
+    while optionValid == False:
+        try:
+            choice = int(input('Option Selected: '))
+            if (choice >= lower) and (choice <= upper):
+                optionValid = True
+            else:
+                print('Please enter a valid option')
+        except ValueError:
+            print('Please enter a valid option')
+    return choice
+
+def display_animal_menu():
+	print()
+	print("Which animal type would you like to add?")
+	print()
+	print("1. Cow")
+	print("2. Sheep")
+	print()
+	print("0. I don't want to add an animal - return me to the main menu")
+	print()
+	print('Please select an option from the above menu')
+
+def manage_field(self):
+	print('This is field management program')
+	print()
+	exit = False
+	while not exit:
+		display_main_menu()
+		option = get_menu_choice(0,7)
+		print()
+		if option == 1:
+			plant_crop_in_field(self)
+		if option == 2:
+			removed_crop = harvest_crop_from_field(self)
+			print("You removed the crop: {0}".format(removed_crop))
+		if option == 3:
+			add_animal_to_field(self)
+		if option == 4:
+			removed_animal = remove_animal_from_field(self)
+			print("You removed the animal: {0}".format(removed_animal))
+		if option == 5:
+			manual_grow(self)
+			print()
+		elif option == 6:
+			auto_grow(self,30)
+			print()
+		elif option == 7:
+			print(self.report_contents())
+			print()
+		elif option == 0:
+			exit = True
+			print()
+	print('Thank you for using the field management program')
+
 def main():
-	#testing
+	#instaniate the class
 	new_field = Field(2,5)
-	new_field.plant_crop(Wheat())
-	new_field.plant_crop(Potato())
-	new_field.add_animal(Cow("Bob"))
-	new_field.add_animal(Sheep("Sally"))
-	print(new_field.report_contents())
-	print(new_field.report_needs())
-	auto_grow(new_field,10)
-	print(new_field.report_contents())
+	#test to see whether it works or not
+	manage_field(new_field)
 
 if __name__ == '__main__':
 	main()
