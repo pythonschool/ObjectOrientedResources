84a915e9fced0cf6bf8fdf1be946715e2c5214ee
Adam McNicol <adamsorders@mac.com>
2013-02-24 11:37:36 +0000

    convert indentation to spaces
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- crop_class.py
+++ crop_class.py
@@ -1,30 +1,30 @@
 class Crop:
-	"""A generic food crop"""
+    """A generic food crop"""
 
-	#constructor
-	def __init__(self,growth_rate,light_need,water_need):
-		#set the attributes with an initial value
+    #constructor
+    def __init__(self,growth_rate,light_need,water_need):
+        #set the attributes with an initial value
 
-		self._growth       = 0
-		self._days_growing = 0
-		self._growth_rate  = growth_rate
-		self._light_need   = light_need
-		self._water_need   = water_need
-		self._status       = "Seed"
-		self._type         = "Generic"
+        self._growth       = 0
+        self._days_growing = 0
+        self._growth_rate  = growth_rate
+        self._light_need   = light_need
+        self._water_need   = water_need
+        self._status       = "Seed"
+        self._type         = "Generic"
 
-		#the above attributes are prefixed with an underscore to indicate that they should not be
-		#accessed directly from outwith the class
+        #the above attributes are prefixed with an underscore to indicate that they should not be
+        #accessed directly from outwith the class
 
 
 def main():
-	#instaniate the class
-	new_crop = Crop(1,4,3)
-	#test to see whether it works or not
-	print(new_crop._status)
-	print(new_crop._light_need)
-	print(new_crop._water_need)
+    #instaniate the class
+    new_crop = Crop(1,4,3)
+    #test to see whether it works or not
+    print(new_crop._status)
+    print(new_crop._light_need)
+    print(new_crop._water_need)
 
 
 if __name__ == '__main__':
-	main()
+    main()
