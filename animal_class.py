dd840533252d9c1cecdb229f8780050be9acab40
Adam McNicol <adamsorders@mac.com>
2013-03-29 15:17:25 +0000

    tabs to spaces
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- animal_class.py
+++ animal_class.py
@@ -1,84 +1,84 @@
 import random
 
 class Animal:
-	"""a generic animal"""
+    """a generic animal"""
 
-	#constructor
-	def __init__(self,growth_rate,food_need,water_need,name):
-		#set the attributes with an initial value
+    #constructor
+    def __init__(self,growth_rate,food_need,water_need,name):
+        #set the attributes with an initial value
 
-		self._weight = 0
-		self._days_growing = 0
-		self._growth_rate = growth_rate
-		self._food_need = food_need
-		self._water_need = water_need
-		self._status = "Baby"
-		self._type = "Generic"
-		self.name = name
+        self._weight = 0
+        self._days_growing = 0
+        self._growth_rate = growth_rate
+        self._food_need = food_need
+        self._water_need = water_need
+        self._status = "Baby"
+        self._type = "Generic"
+        self.name = name
 
-		#the above attributes are prefixed with an underscore to indicate that they should not be
-		#accessed directly from outwith the class except for name which can be changed freely
+        #the above attributes are prefixed with an underscore to indicate that they should not be
+        #accessed directly from outwith the class except for name which can be changed freely
 
-	#method to indicate the needs of the animal
-	def needs(self):
-		#return a dictionary containing the food and water needs
-		return {'food need':self._food_need,'water need':self._water_need}
+    #method to indicate the needs of the animal
+    def needs(self):
+        #return a dictionary containing the food and water needs
+        return {'food need':self._food_need,'water need':self._water_need}
 
-	#method to provide information about the current state of the animal
-	def report(self):
-		#return a dictionary containing name,type,status, weight and days growing
-		return {'name':self.name,'type':self._type,'status':self._status,'weight':self._weight,'days growing':self._days_growing}
+    #method to provide information about the current state of the animal
+    def report(self):
+        #return a dictionary containing name,type,status, weight and days growing
+        return {'name':self.name,'type':self._type,'status':self._status,'weight':self._weight,'days growing':self._days_growing}
 
-	#the underscore indicates that this method should not be called from outwith the class
-	def _update_status(self):
-		if self._weight > 30:
-			self._status = "Prime"
-		elif self._weight > 15:
-			self._status = "Fine"
-		elif self._weight > 10:
-			self._status = "Poor"
-		elif self._weight >= 0:
-			self._status = "Baby"
+    #the underscore indicates that this method should not be called from outwith the class
+    def _update_status(self):
+        if self._weight > 30:
+            self._status = "Prime"
+        elif self._weight > 15:
+            self._status = "Fine"
+        elif self._weight > 10:
+            self._status = "Poor"
+        elif self._weight >= 0:
+            self._status = "Baby"
 
-	def grow(self,food,water):
-		if food >= self._food_need and water >= self._water_need:
-			self._weight += self._growth_rate
-		#increment days growing
-		self._days_growing += 1
-		#update the status
-		self._update_status()
+    def grow(self,food,water):
+        if food >= self._food_need and water >= self._water_need:
+            self._weight += self._growth_rate
+        #increment days growing
+        self._days_growing += 1
+        #update the status
+        self._update_status()
 
 def manual_grow(self):
-	#get the food and water values from the user
-	valid = False
-	while not valid:
-		try:
-			food = int(input("Please enter a food value (1-10): "))
-			if 1 <= food <= 10:
-				valid = True
-			else:
-				print("Value entered not valid - please enter a value between 1 and 10")
-		except ValueError:
-			print("Value entered not valid - please enter a value between 1 and 10")
-	valid = False
-	while not valid:
-		try:
-			water = int(input("Please enter a water value (1-10): "))
-			if 1 <= water <= 10:
-				valid = True
-			else:
-				print("Value entered not valid - please enter a value between 1 and 10")
-		except ValueError:
-			print("Value entered not valid - please enter a value between 1 and 10")
-	#grow the animal
-	self.grow(food,water)
+    #get the food and water values from the user
+    valid = False
+    while not valid:
+        try:
+            food = int(input("Please enter a food value (1-10): "))
+            if 1 <= food <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")
+    valid = False
+    while not valid:
+        try:
+            water = int(input("Please enter a water value (1-10): "))
+            if 1 <= water <= 10:
+                valid = True
+            else:
+                print("Value entered not valid - please enter a value between 1 and 10")
+        except ValueError:
+            print("Value entered not valid - please enter a value between 1 and 10")
+    #grow the animal
+    self.grow(food,water)
 
 def auto_grow(self,days):
-	#grow the animal automatically over 30 days
-	for day in range(days):
-		food = random.randint(1,10)
-		water = random.randint(1,10)
-		self.grow(food,water)
+    #grow the animal automatically over 30 days
+    for day in range(days):
+        food = random.randint(1,10)
+        water = random.randint(1,10)
+        self.grow(food,water)
 
 def displayMenu():
     print('1. Grow manually over 1 day')
@@ -102,32 +102,32 @@ def getMenuChoice():
     return choice
 
 def manage_animal(self):
-	print('This is animal management program')
-	print()
-	noexit = True
-	while noexit:
-		displayMenu()
-		option = getMenuChoice()
-		print()
-		if option == 1:
-			manual_grow(self)
-			print()
-		elif option == 2:
-			auto_grow(self,30)
-			print()
-		elif option == 3:
-			print(self.report())
-			print()
-		elif option == 0:
-			noexit = False
-			print()
-	print('Thank you for using the animal management program')
+    print('This is animal management program')
+    print()
+    noexit = True
+    while noexit:
+        displayMenu()
+        option = getMenuChoice()
+        print()
+        if option == 1:
+            manual_grow(self)
+            print()
+        elif option == 2:
+            auto_grow(self,30)
+            print()
+        elif option == 3:
+            print(self.report())
+            print()
+        elif option == 0:
+            noexit = False
+            print()
+    print('Thank you for using the animal management program')
 
 def main():
-	#instaniate the class
-	new_animal = Animal(1,4,5,"Sally")
-	#test to see whether it works or not
-	manage_animal(new_animal)
+    #instaniate the class
+    new_animal = Animal(1,4,5,"Sally")
+    #test to see whether it works or not
+    manage_animal(new_animal)
 
 if __name__ == '__main__':
-	main()
+    main()
--- field_class.py
+++ field_class.py
@@ -4,41 +4,41 @@ from cow_class import *
 from sheep_class import *
 
 class Field:
-	"""Simulate a field that can contain animals and crops"""
+    """Simulate a field that can contain animals and crops"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		self._crops = []
-		self._animals = []
-		self._max_animals = max_animals
-		self._max_crops = max_crops
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        self._crops = []
+        self._animals = []
+        self._max_animals = max_animals
+        self._max_crops = max_crops
 
-	def plant_crop(self,crop):
-		if len(self._crops) < self._max_crops:
-			self._crops.append(crop)
-			return True
-		else:
-			return False
+    def plant_crop(self,crop):
+        if len(self._crops) < self._max_crops:
+            self._crops.append(crop)
+            return True
+        else:
+            return False
 
-	def add_animal(self,animal):
-		if len(self._animals) < self._max_animals:
-			self._animals.append(animal)
-			return True
-		else:
-			return False
+    def add_animal(self,animal):
+        if len(self._animals) < self._max_animals:
+            self._animals.append(animal)
+            return True
+        else:
+            return False
 
 
 
 def main():
-	#testing
-	new_field = Field(2,5)
-	new_field.plant_crop(Wheat())
-	new_field.plant_crop(Potato())
-	new_field.add_animal(Sheep("Shaun"))
-	print(new_field._crops)
-	print(new_field._animals)
+    #testing
+    new_field = Field(2,5)
+    new_field.plant_crop(Wheat())
+    new_field.plant_crop(Potato())
+    new_field.add_animal(Sheep("Shaun"))
+    print(new_field._crops)
+    print(new_field._animals)
 
 if __name__ == '__main__':
-	main()
+    main()
 
-	
+    
