1a9f13ca83a3678f95131e71dd83f146df46f71b
Adam McNicol <adamsorders@mac.com>
2013-03-29 15:20:52 +0000

    tabs to spaces
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- field_class.py
+++ field_class.py
@@ -4,58 +4,58 @@ from cow_class import *
 from sheep_class import *
 
 class Field:
-	"""Simulate a field that can contain animals and crops"""
+    """Simulate a field that can contain animals and crops"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		self._crops = []
-		self._animals = []
-		self._max_animals = max_animals
-		self._max_crops = max_crops
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        self._crops = []
+        self._animals = []
+        self._max_animals = max_animals
+        self._max_crops = max_crops
 
-	def plant_crop(self,crop):
-		if len(self._crops) < self._max_crops:
-			self._crops.append(crop)
-			return True
-		else:
-			return False
+    def plant_crop(self,crop):
+        if len(self._crops) < self._max_crops:
+            self._crops.append(crop)
+            return True
+        else:
+            return False
 
-	def add_animal(self,animal):
-		if len(self._animals) < self._max_animals:
-			self._animals.append(animal)
-			return True
-		else:
-			return False
+    def add_animal(self,animal):
+        if len(self._animals) < self._max_animals:
+            self._animals.append(animal)
+            return True
+        else:
+            return False
 
-	def harvest_crop(self,position):
-		return self._crops.pop(position)
+    def harvest_crop(self,position):
+        return self._crops.pop(position)
 
-	def remove_animal(self,position):
-		return self._animals.pop(position)
+    def remove_animal(self,position):
+        return self._animals.pop(position)
 
 def display_crops(crop_list):
-	print()
-	print("The following crops are in this field:")
-	for pos in range(len(crop_list)):
-		print("{0:>4}. {1}.".format(pos+1,crop_list[pos].report()))
+    print()
+    print("The following crops are in this field:")
+    for pos in range(len(crop_list)):
+        print("{0:>4}. {1}.".format(pos+1,crop_list[pos].report()))
 
 def display_animals(animal_list):
-	print("The following animals are in this field:")
-	for pos in range(len(animal_list)):
-		print("{0:>4}. {1}.".format(pos+1,animal_list[pos].report()))
+    print("The following animals are in this field:")
+    for pos in range(len(animal_list)):
+        print("{0:>4}. {1}.".format(pos+1,animal_list[pos].report()))
 
 
 
 def main():
-	#testing
-	new_field = Field(2,5)
-	new_field.plant_crop(Wheat())
-	new_field.plant_crop(Potato())
-	new_field.add_animal(Sheep("Shaun"))
-	display_crops(new_field._crops)
-	display_animals(new_field._animals)
+    #testing
+    new_field = Field(2,5)
+    new_field.plant_crop(Wheat())
+    new_field.plant_crop(Potato())
+    new_field.add_animal(Sheep("Shaun"))
+    display_crops(new_field._crops)
+    display_animals(new_field._animals)
 
 if __name__ == '__main__':
-	main()
+    main()
 
-	
+    
--- sheep_class.py
+++ sheep_class.py
@@ -1,31 +1,31 @@
 from animal_class import *
 
 class Sheep(Animal):
-	"""A simulation of a Sheep"""
+    """A simulation of a Sheep"""
 
-	#constructor
-	def __init__(self,name):
-		#call the parent class constructor with default values for Sheep
-		#growth rate = 3; food need = 3, water need = 2
-		super().__init__(3,3,2,name)
-		self._type = "Sheep" #override the parent class attribute with new value
+    #constructor
+    def __init__(self,name):
+        #call the parent class constructor with default values for Sheep
+        #growth rate = 3; food need = 3, water need = 2
+        super().__init__(3,3,2,name)
+        self._type = "Sheep" #override the parent class attribute with new value
 
-	#override the grow method from the parent class
-	def grow(self,food,water):
-		if food > self._food_need and water > self._water_need:
-			self._weight += self._growth_rate * 1.2
-		elif food == self._food_need and water == self._water_need:
-			self._weight += self._growth_rate
-		#increment days growing
-		self._days_growing += 1
-		#update the status
-		self._update_status()
+    #override the grow method from the parent class
+    def grow(self,food,water):
+        if food > self._food_need and water > self._water_need:
+            self._weight += self._growth_rate * 1.2
+        elif food == self._food_need and water == self._water_need:
+            self._weight += self._growth_rate
+        #increment days growing
+        self._days_growing += 1
+        #update the status
+        self._update_status()
 
 def main():
-	#instaniate the class
-	new_Sheep = Sheep("Shaun")
-	#test to see whether it works or not
-	manage_animal(new_Sheep)
+    #instaniate the class
+    new_Sheep = Sheep("Shaun")
+    #test to see whether it works or not
+    manage_animal(new_Sheep)
 
 if __name__ == '__main__':
-	main()
+    main()
