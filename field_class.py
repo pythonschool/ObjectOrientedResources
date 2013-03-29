b52eeeb6c21338cb72559c849382a29268bed6dd
Adam McNicol <adamsorders@mac.com>
2013-03-29 15:15:35 +0000

    tabs to spaces
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- field_class.py
+++ field_class.py
@@ -3,20 +3,20 @@ from wheat_class import *
 from animal_class import *
 
 class Field:
-	"""Simulate a field that can contain animals and crops"""
+    """Simulate a field that can contain animals and crops"""
 
-	#constructor
-	def __init__(self,max_animals,max_crops):
-		self._crops = []
-		self._animals = []
-		self._max_animals = max_animals
-		self._max_crops = max_crops
+    #constructor
+    def __init__(self,max_animals,max_crops):
+        self._crops = []
+        self._animals = []
+        self._max_animals = max_animals
+        self._max_crops = max_crops
 
 
 def main():
-	pass
+    pass
 
 if __name__ == '__main__':
-	main()
+    main()
 
-	
+    
