2548fb792cd1714c5b4966be6e3b786b24ade06d
Adam McNicol <adamsorders@mac.com>
2013-03-29 15:23:10 +0000

    tabs to spaces
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- field_class.py
+++ field_class.py
@@ -4,83 +4,83 @@ from cow_class import *
 from sheep_class import *
 
 class Field:
-	"""Simulate a field that can contain animals and crops"""
+    """Simulate a field that can contain animals and crops"""
 
-	#constructor
-	def __init__(self,max_crops,max_animals):
-		self._crops = []
-		self._animals = []
-		self._max_animals = max_animals
-		self._max_crops = max_crops
+    #constructor
+    def __init__(self,max_crops,max_animals):
+        self._crops = []
+        self._animals = []
+        self._max_animals = max_animals
+        self._max_crops = max_crops
 
-	def plant_crop(self,crop):
-		if len(self._crops) < self._max_crops:
-			self._crops.append(crop)
-			return True
-		else:
-			return False
+    def plant_crop(self,crop):
+        if len(self._crops) < self._max_crops:
+            self._crops.append(crop)
+            return True
+        else:
+            return False
 
-	def add_animal(self,animal):
-		if len(self._animals) < self._max_animals:
-			self._animals.append(animal)
-			return True
-		else:
-			return False
+    def add_animal(self,animal):
+        if len(self._animals) < self._max_animals:
+            self._animals.append(animal)
+            return True
+        else:
+            return False
 
-	def harvest_crop(self,position):
-		return self._crops.pop(position)
+    def harvest_crop(self,position):
+        return self._crops.pop(position)
 
-	def remove_animal(self,position):
-		return self._animals.pop(position)
+    def remove_animal(self,position):
+        return self._animals.pop(position)
 
 def display_crops(crop_list):
-	print()
-	print("The following crops are in this field:")
-	for pos in range(len(crop_list)):
-		print("{0:>4}. {1}.".format(pos+1,crop_list[pos].report()))
+    print()
+    print("The following crops are in this field:")
+    for pos in range(len(crop_list)):
+        print("{0:>4}. {1}.".format(pos+1,crop_list[pos].report()))
 
 def display_animals(animal_list):
-	print("The following animals are in this field:")
-	for pos in range(len(animal_list)):
-		print("{0:>4}. {1}.".format(pos+1,animal_list[pos].report()))
+    print("The following animals are in this field:")
+    for pos in range(len(animal_list)):
+        print("{0:>4}. {1}.".format(pos+1,animal_list[pos].report()))
 
 def select_crop(length_list):
-	selected = 0
-	while selected not in range(1,length_list+1):
-		selected = int(input("Please select a crop: "))
-		if selected not in range(1,length_list+1):
-			print("Please select a valid option")
-	return selected - 1
+    selected = 0
+    while selected not in range(1,length_list+1):
+        selected = int(input("Please select a crop: "))
+        if selected not in range(1,length_list+1):
+            print("Please select a valid option")
+    return selected - 1
 
 def select_animal(length_list):
-	selected = 0
-	while selected not in range(1,length_list+1):
-		selected = int(input("Please select an animal: "))
-		if selected not in range(1,length_list+1):
-			print("Please select a valid option")
-	return selected - 1
+    selected = 0
+    while selected not in range(1,length_list+1):
+        selected = int(input("Please select an animal: "))
+        if selected not in range(1,length_list+1):
+            print("Please select a valid option")
+    return selected - 1
 
 def harvest_crop_from_field(new_field):
-	display_crops(new_field._crops)
-	selected_crop = select_crop(len(new_field._crops))
-	return new_field.harvest_crop(selected_crop)
+    display_crops(new_field._crops)
+    selected_crop = select_crop(len(new_field._crops))
+    return new_field.harvest_crop(selected_crop)
 
 def remove_animal_from_field(new_field):
-	display_animals(new_field._animals)
-	selected_animal = select_animal(len(new_field._animals))
-	return new_field.remove_animal(selected_animal)
+    display_animals(new_field._animals)
+    selected_animal = select_animal(len(new_field._animals))
+    return new_field.remove_animal(selected_animal)
 
 def main():
-	#testing
-	new_field = Field(2,5)
-	new_field.plant_crop(Wheat())
-	new_field.plant_crop(Potato())
-	new_field.add_animal(Sheep("Shaun"))
-	print(new_field._crops)
-	print(harvest_crop_from_field(new_field))
-	print(new_field._crops)
+    #testing
+    new_field = Field(2,5)
+    new_field.plant_crop(Wheat())
+    new_field.plant_crop(Potato())
+    new_field.add_animal(Sheep("Shaun"))
+    print(new_field._crops)
+    print(harvest_crop_from_field(new_field))
+    print(new_field._crops)
 
 if __name__ == '__main__':
-	main()
+    main()
 
-	
+    
--- wheat_class.py
+++ wheat_class.py
@@ -1,40 +1,40 @@
 from crop_class import *
 
 class Wheat(Crop):
-	"""A wheat crop"""
+    """A wheat crop"""
 
-	#constructor
-	def __init__(self):
-		#call the parent class constructor with default values for Wheat
-		#growth rate = 2; light need = 5, water need = 4
-		super().__init__(2,5,4)
-		self._type = "Wheat" #override the parent class attribute with new value
+    #constructor
+    def __init__(self):
+        #call the parent class constructor with default values for Wheat
+        #growth rate = 2; light need = 5, water need = 4
+        super().__init__(2,5,4)
+        self._type = "Wheat" #override the parent class attribute with new value
 
-	def grow(self,light,water):
-		if light >= self._light_need and water >= self._water_need:
-			if self._status == "Seedling":
-				self._growth += self._growth_rate * 1.5
-			elif self._status == "Young":
-				self._growth += self._growth_rate *1.25
-			elif self._status == "Old":
-				self._growth += self._growth_rate / 2
-			else:
-				self._growth += self._growth_rate
-		#increment days growing
-		self._days_growing += 1
-		#update the status
-		self._update_status()
+    def grow(self,light,water):
+        if light >= self._light_need and water >= self._water_need:
+            if self._status == "Seedling":
+                self._growth += self._growth_rate * 1.5
+            elif self._status == "Young":
+                self._growth += self._growth_rate *1.25
+            elif self._status == "Old":
+                self._growth += self._growth_rate / 2
+            else:
+                self._growth += self._growth_rate
+        #increment days growing
+        self._days_growing += 1
+        #update the status
+        self._update_status()
 
 
 def main():
-	#create a new wheat crop
-	wheat_crop = Wheat()
-	print(wheat_crop.report())
-	#manually grow the crop
-	manual_grow(wheat_crop)
-	print(wheat_crop.report())
-	auto_grow(wheat_crop,15)
-	print(wheat_crop.report())
+    #create a new wheat crop
+    wheat_crop = Wheat()
+    print(wheat_crop.report())
+    #manually grow the crop
+    manual_grow(wheat_crop)
+    print(wheat_crop.report())
+    auto_grow(wheat_crop,15)
+    print(wheat_crop.report())
 
 if __name__ == '__main__':
-	main()
+    main()
